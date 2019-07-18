3651ff5eeef8fcba7ac93d7b39f0cf8aa9e44cfb
Wiiliam D C Santana <williamdiegocampos@gmail.com>
2019-07-18 18:15:44 -0300

    Criacao do componente

---
+++ src/main/java/br/estudo/springbootrestpgmnts/SpringbootRestPgmntsApplication.java
@@ -0,0 +1,13 @@
+package br.estudo.springbootrestpgmnts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestPgmntsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestPgmntsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/estudo/springbootrestpgmnts/SpringbootRestPgmntsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.estudo.springbootrestpgmnts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestPgmntsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
